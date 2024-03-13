ebd143557ec9267018ba08cdbc834b8bcd8ac0c5
Abril <abril927@proton.me>
2024-03-13 18:23:54 +0100

    Initial version of file metadata microservice
    passes tests and stuff

--- src/main.ts
+++ src/main.ts
@@ -4,29 +4,24 @@ dotenv.config();
 import { default as express, Express } from 'express';
 import { default as cors } from 'cors';
 
+import { default as path } from 'path';
+
 let app = express();
 app.use(cors({ optionsSuccessStatus: 200 }));
 
+import { default as multer } from 'multer';
+const upload = multer({ dest: path.join(__dirname, '../data/uploads') });
+
 app.get('/', (req, res) => {
-	res.send('API: /api/:date');
+	res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
-app.get('/api/:date?', (req, res) => {
-	const date = req.params.date;
-	let parsed: Date;
-	if (date != null) {
-		parsed = new Date(date);
-		if (isNaN(parsed.getTime())) {
-			parsed = new Date(parseInt(date));
-			if (isNaN(parsed.getTime())) {
-				return res.json({ error: 'Invalid Date' });
-			}
-		}
-	} else {
-		parsed = new Date();
-	}
-	
-	return res.json({ unix: parsed.getTime(), utc: parsed.toUTCString() });
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {	
+	return res.json({ 
+		name: req.file.originalname,
+		type: req.file.mimetype,
+		size: req.file.size
+	});
 });
 
 app.listen(process.env.PORT || 3000, () => {
